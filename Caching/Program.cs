bf897a193c20e7658d64624810e130becccce591
wassem7 <waseemdarkwa@gmail.com>
2023-12-28 12:35:18 +0000

    Cache Service added to DI container

--- Caching/Program.cs
+++ Caching/Program.cs
@@ -1,12 +1,19 @@
 using Caching.Data;
+using Caching.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ApplicationDbContext>(opt =>
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+builder.Services.AddScoped<ICacheService, CacheService>();
+builder
+    .Services
+    .AddEntityFrameworkNpgsql()
+    .AddDbContext<ApplicationDbContext>(
+        opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString"))
+    );
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
